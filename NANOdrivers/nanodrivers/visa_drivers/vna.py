ac43d1caa550a493258d54055a18558a258dc927
Demag <mukhanova.ed@gmail.com>
2024-01-21 15:31:09 +0200

    Dump param function is created

--- NANOdrivers/nanodrivers/visa_drivers/vna.py
+++ NANOdrivers/nanodrivers/visa_drivers/vna.py
@@ -1,3 +1,5 @@
+from __future__ import (absolute_import, division, print_function)
+
 from numpy import *
 import numpy as np
 import nanodrivers.visa_drivers.visa_dev as v
@@ -6,12 +8,32 @@ import nanodrivers.visa_drivers.global_settings as gs
 
 global_vna_address = gs.vna_address
 
+
 def magtodb(mag):
     return 20 * np.log10(mag)  # applied to power measurement
 
 
 def dbtomag(db):
-    return np.pow(10, db/20)  # applied to power measurement
+    return np.pow(10, db / 20)  # applied to power measurement
+
+
+def get_class_attributes(print_it=False):
+    """
+    Function returns all class attributes
+
+    Returns: list of all variables globally defined within class
+
+    """
+    list_of_att = dict()
+    for attribute in VNA.__dict__.keys():
+        if attribute[:2] != '__':
+            value = getattr(VNA, attribute)
+            if not callable(value):
+                list_of_att[attribute] = value
+                if print_it:
+                    print(attribute, '=', value)
+
+    return list_of_att
 
 
 class VNA(v.BaseVisa):
@@ -31,22 +53,71 @@ class VNA(v.BaseVisa):
      """
     def __init__(self, device_num=global_vna_address, form=0):
         super().__init__(device_num)
+        self.type = None
+        self.get_sweep_type()
+
         self.form = form
         self.write('INIT1:CONT OFF')  # single sweep mode
         self.write("CALC1:FORM MLOG")  # set data format to
+
+        self.cent_freq = None
+        self.span = None
+        self.star_freq = None
+        self.stop_freq = None
+        self.cw_freq = None
+
+        self.status = None
+
+        self.nop = None
+        self.get_nop()
+
+        self.band = None
+        self.get_band()
+
+        self.power = None
+        self.get_power()
+
+    def get_instance_attributes(self, print_it=False):
+        """
+        Function returns all pre-defined class attributes
+
+        Returns: list of all variables defined in class __init__
+
+        """
+        list_of_att = dict()
+        for attribute, value in self.__dict__.items():
+            list_of_att[attribute] = value
+            if print_it:
+                print(attribute, '=', value)
+        return list_of_att
+
+    def sys_help(self):
+        """
+        Build-in help command
+
+        Args: None
+        Returns: Full list of commands available for the device
+
+        """
+        return print(self.query('SYSTem:HELP:HEAD?'))
+
+    def get_sweep_type(self):
+        self.type = self.query('SENS1:SWE:TYPE?')[:-1]
+        return self.type
 
     def get_data(self):
         """
         Readout in CW mode. After initialisation of the measurements a pause need to be set.
         The pause = sweep time + 0.2 s. Otherwise readout request will come before measurement ends
         """
-        self.on()
+
+        self.set_on()
         self.write("INIT1:IMM")
-        time.sleep(0.2 + self.ask_sweep_time())
+        time.sleep(0.2 + self.get_sweep_time())
         data_str = self.query("CALC1:DATA? SDAT")
         data = np.array(data_str.rstrip().split(",")).astype("float64")
         s = data[0::2] + 1j * data[1::2]
-        self.off()
+        self.set_off()
 
         if self.form == 0:
             # print('WARNING: CHECK ANGLE (rad or deg)!')
@@ -63,65 +134,144 @@ class VNA(v.BaseVisa):
         elif self.form == 4:
             return data[0::2], data[1::2]
         elif self.form == 5:
-            return s
+            return
 
-    def on(self):
+    def get_sweep_time(self):
+        return self.query_float('SENS1:SWE:TIME?')
+
+    def get_power(self):
+        self.power = self.query_float('SOUR1:POW?')
+        return self.power
+
+    def get_band(self):
+        self.band = self.query_float(':SENS1:BAND?')
+        return self.band
+
+    def get_nop(self):
+        self.nop = int(self.query_float('SENS1:SWE:POIN?'))
+        return self.nop
+
+    def get_start_freq(self):
+        self.star_freq = self.query_float(':SENS1:FREQ:STAR?')
+        return self.star_freq
+
+    def get_stop_freq(self):
+        self.stop_freq = self.query_float(':SENS1:FREQ:STOP?')
+        return self.stop_freq
+
+    def get_cent_freq(self):
+        self.cent_freq = self.query_float(':SENS1:FREQ:CENT?')
+        return self.cent_freq
+
+    def get_freq(self):
+        """
+        Function to get frequency sweep array
+        Args: None
+
+        Returns: freq array
+
+        """
+
+        self.star_freq = self.get_start_freq()
+        self.stop_freq = self.get_stop_freq()
+        self.nop = self.get_nop()
+        freq = np.linspace(self.star_freq, self.stop_freq, self.nop)
+        return freq
+
+    def get_status(self):
+        self.status = self.query_int('OUTP?')
+        return self.status
+
+    def set_lin(self):
+        self.write('SENS1:SWE:TYPE LIN')
+        self.type = self.get_sweep_type()
+        return self.type
+
+    def set_cw(self):
+        self.write('SENS1:SWE:TYPE CW')
+        self.type = self.get_sweep_type()
+        return self.type
+
+    def set_on(self):
         self.write('OUTP ON')
 
-    def off(self):
+    def set_off(self):
         self.write('OUTP OFF')
-
-    def ask_nop(self):
-        return self.query_int('SENS1:SWE:POIN?')
-
-    def ask_sweep_time(self):
-        return self.query_float('SENS1:SWE:TIME?')
 
     def set_cw_freq(self, freq):
         """
         Set single frequency point for CW mode in Hz
         """
-        self.write(':SENS1:SWE:TYPE CW')   # change to CW mode
+
+        self.write(':SENS1:SWE:TYPE CW')  # change to CW mode
         if freq < 100:
             print("Warning: probably frequency range is GHz, but Hz needed. Frequency will be converted to Hz")
-            freq = freq*1e9
-        self.write('SENS1:FREQ:CW {}'.format(str(freq)))
+            freq = freq * 1e9
+        self.cw_freq = freq
+        self.write('SENS1:FREQ:CW {}'.format(str(self.cw_freq)))
 
-    def set_freq_start_stop(self, start_fr, stop_fr):
+    def set_start_freq(self, start_fr):
+        self.star_freq = start_fr
+        self.write('SENS1:FREQ: STAR {}'.format(str(self.star_freq)))
+
+    def set_stop_freq(self, stop_fr):
+        self.stop_freq = stop_fr
+        self.write('SENS1:FREQ: STOP {}'.format(str(self.stop_freq)))
+
+    def set_span(self, span):
+        self.span = span
+        self.write('SENS1:FREQ: SPAN {}'.format(str(self.span)))
+
+    def set_cent_freq(self, cent_fr):
+        self.cent_freq = cent_fr
+        self.write('SENS1:FREQ: CENT {}'.format(str(self.cent_freq)))
+
+    def set_freq_start_stop(self, start_fr, stop_fr, nop):
         """
         Set frequency sweep in Hz with start-stop-number_of_points setup
         """
 
         if start_fr < 100:
             print("Warning: probably frequency range is GHz, but Hz needed. Frequency will be converted to Hz")
-            start_fr = start_fr*1e9
+            start_fr = start_fr * 1e9
         if stop_fr < 100:
             print("Warning: probably frequency range is GHz, but Hz needed. Frequency will be converted to Hz")
-            stop_fr = stop_fr*1e9
+            stop_fr = stop_fr * 1e9
 
-        self.write('SENS1:FREQ: STAR {}'.format(str(start_fr)))
-        self.write('SENS1:FREQ: STOP {}'.format(str(stop_fr)))
+        self.star_freq = start_fr
+        self.stop_freq = stop_fr
+        self.nop = nop
 
-    def set_freq_cent_span(self, start_cent, span):
+        self.set_nop(self.nop)
+        self.set_start_freq(self.star_freq)
+        self.set_stop_freq(self.stop_freq)
+
+    def set_freq_cent_span(self, cent_fr, span, nop):
         """
         Set frequency sweep in Hz with start-stop-number_of_points setup
         """
 
         if start_cent < 100:
             print("Warning: probably frequency range is GHz, but Hz needed. Frequency will be converted to Hz")
-            start_cent = start_cent * 1e9
+            start_cent = cent_fr * 1e9
         if span < 100:
             print("Warning: probably frequency range is GHz, but Hz needed. Frequency will be converted to Hz")
             span = span * 1e9
 
-        self.write('SENS1:FREQ: CENT {}'.format(str(start_cent)))
-        self.write('SENS1:FREQ: SPAN {}'.format(str(span)))
+        self.cent_freq = cent_fr
+        self.span = span
+        self.nop = nop
+
+        self.set_nop(self.nop)
+        self.set_cent_freq(self.star_freq)
+        self.set_span(self.stop_freq)
 
     def set_power(self, meas_power):
         if meas_power >= 10:
             print('Too high power! Power=10 will be set')
             meas_power = 10
-        self.write('SOUR1:POW {}'.format(str(meas_power)))
+        self.power = meas_power
+        self.write('SOUR1:POW {}'.format(str(self.power)))
 
     def set_band(self, bandwidth):
         """
@@ -130,10 +280,12 @@ class VNA(v.BaseVisa):
         {1, 1.5, 2, 3, 5, 7}·pow(10,n) Hz for (n ≥ 0).
         Values exceeding the maximum bandwidth are rounded down.
         """
-        self.write(':SENS1:BAND {}'.format(str(bandwidth)))
+        self.band = bandwidth
+        self.write(':SENS1:BAND {}'.format(str(self.band)))
 
     def set_nop(self, nop):
-        self.write('SENS1:SWE:POIN {}'.format(str(nop)))
+        self.nop = nop
+        self.write('SENS1:SWE:POIN {}'.format(str(self.nop)))
 
     def cw_meas(self, freq=6e9, meas_power=-10, band=10):
         """
@@ -166,5 +318,3 @@ class VNA(v.BaseVisa):
         self.set_power(meas_power)
 
         return self.get_data()
-
-
