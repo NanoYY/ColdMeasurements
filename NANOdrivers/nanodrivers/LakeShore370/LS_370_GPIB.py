a0bee850c88d951eaa295b5ae3b42e65a2f511a9
NanoGroup <mukhanova.ed@gmail.com>
2024-02-11 00:30:24 +0200

    Lakeshore GPIB

--- NANOdrivers/nanodrivers/LakeShore370/LS_370_GPIB.py
+++ NANOdrivers/nanodrivers/LakeShore370/LS_370_GPIB.py
@@ -41,7 +41,12 @@ class LakeShore(v.BaseVisa):
     def __init__(self, device_num=global_ls_address):
         super().__init__(device_num)
 
-        self.PID = None
+        self.PID = np.array([nan, nan, nan])
+
+        self.channel_temp = np.array([nan, nan, nan, nan, nan, nan, nan, nan])
+
+        for i in [1, 2, 5, 6]:
+            self.get_temp(i)
 
     def dump(self, print_it=False):
         """
@@ -60,7 +65,7 @@ class LakeShore(v.BaseVisa):
                 list_of_att[attribute] = str(value)
         return list_of_att
 
-    def get_temp(self, channel):
+    def get_temp(self, channel=6):
         """
         Function to get temperature of specific channel
         Args:
@@ -69,4 +74,87 @@ class LakeShore(v.BaseVisa):
         Returns: temperature in Kelvins
 
         """
-        float(ls.query('RDGK? 6')[:-2])
+        self.channel_temp[channel] = float(self.query('RDGK? {}').format(channel)[:-2])
+        return self.channel_temp[channel]
+
+    def get_PID(self):
+        """
+        Function to get PID control
+        Returns: PID parameters
+
+        """
+        sr = self.query('PID?')[:-2].split(',')
+        for i in range(3):
+            self.PID[i] = float(sr[i])
+
+        return self.PID
+
+    def set_setpoint(self, temp, channel=6, sudo_mode=False):
+        """
+        Function to set temperature of specific channel
+        Args:
+            temp: temperature in Kelvins
+            channel: channel number [1..6]
+            sudo_mode: If this mode is used, no restriction will be applied. Be careful.
+        Returns: None
+
+        """
+        if temp<1.4:
+            self.channel_temp[channel] = temp
+            self.write('SETP {}'.format(str(self.channel_temp[channel])))
+            return True
+        elif sudo_mode:
+            print('WARNING! Temperature might be too high: {} K'.format(temp))
+            print('However, the temperature {} K will be set as new set point.'.format(temp))
+            self.channel_temp[channel] = temp
+            self.write('SETP {}'.format(str(self.channel_temp[channel])))
+            return True
+        else:
+            print('WARNING! Temperature is too high: {} K'.format(temp))
+            print('The temperature {} K will NOT be set as new set point.'.format(temp))
+            return False
+
+    def set_new_temperature(self, temp):
+        """
+        Function to set new temperature on MC. Uses predefined PID parameters.
+        Main advantage is minimal waiting time. In the future adjustment of PID parameters will be added.
+
+        Args:
+            temp: Temperature in Kelvins
+
+        Returns: True once setting new temperature is finished.
+
+        """
+        initial_temperature = self.get_temp()
+
+        # to prevent overheating when the temperature difference is too high, first set 0.8 of set point.
+
+        delta = temp-initial_temperature
+        if delta<0:
+            self.set_setpoint(temp)
+            print('Just cooling down')
+        else:
+            new_temp1 = initial_temperature + 0.8*delta
+            self.set_setpoint(new_temp1)
+            print('Heats up to initial temperature + 0.8*delta')
+            time.sleep(60*delta*2)
+            new_temp2 = initial_temperature + 0.9 * delta
+            self.set_setpoint(new_temp2)
+            print('Heats up to initial temperature + 0.9*delta')
+            time.sleep(30*delta*2)
+            new_temp3 = initial_temperature + delta
+            self.set_setpoint(new_temp3)
+            print('Heats up to set point')
+            time.sleep(20*delta*2)
+
+        temps = np.array([])
+        for i in range(5):
+            temps = np.append(temps, self.get_temp())
+            time.sleep(10)
+
+        while not(temp-0.001 < np.mean(temps) < temp+0.001):
+            temps = np.delete(temps, 0)
+            temps = np.append(temps, self.get_temp())
+            time.sleep(10)
+
+        return True
--- NANOdrivers/setup.py
+++ NANOdrivers/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "nanodrivers",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Ekaterina Mukhanova",
     author_email = "ekaterina.mukhanova@aalto.fi",
     description = "drivers for scientific microwave equipment",
